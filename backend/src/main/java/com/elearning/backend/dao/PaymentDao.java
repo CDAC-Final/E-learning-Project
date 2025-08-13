d3171b1cc0535f30080a83212087284f75c0cb7b
MohitBidwai <mohit.19bidwai@gmail.com>
2025-08-13 19:52:51 +0530

    Delete backend directory

--- backend/src/main/java/com/elearning/backend/dao/PaymentDao.java
+++
@@ -1,9 +0,0 @@
-package com.elearning.backend.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.elearning.backend.entity.Payment;
-
-public interface PaymentDao extends JpaRepository<Payment, Long> {
-
-}
--- backend/src/main/java/com/elearning/backend/exception/EmailAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.elearning.backend.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-  public EmailAlreadyExistsException(String message)
-  {
-	  super(message);
-  }
-}
